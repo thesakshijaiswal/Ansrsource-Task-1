c04382a5177d1b07debc0b2eca1bfccf93dc1fd5
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-27 14:34:29 +0530

    feat: close small screen menu after clicking on link

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const btnOpen = document.querySelector("#btn-open");
 const btnClose = document.querySelector("#btn-close");
-
 //Show small screen menu to accessibility tools only when the below screen size matches
 const media = window.matchMedia("(width < 900px)");
 const navLinkContainer = document.querySelector(".nav-link-container");
-const main = document.querySelector("main");
-const body = document.querySelector("body");
+const navLinks = document.querySelectorAll("[data-scroll]");
 
 function setUpNav(e) {
   if (e.matches) {
@@ -28,33 +26,34 @@ function openMobileMenu() {
   btnOpen.setAttribute("aria-expanded", "true");
   navLinkContainer.removeAttribute("inert");
   navLinkContainer.removeAttribute("style");
-  main.setAttribute("inert", "");
   btnClose.focus();
-
-  //Disable scrolling when menu is open
-  bodyScrollLockUpgrade.disableBodyScroll(body);
 }
 
 // Close menu
 function closeMobileMenu() {
   btnOpen.setAttribute("aria-expanded", "false");
   navLinkContainer.setAttribute("inert", "");
-  main.removeAttribute("inert");
   btnOpen.focus();
-
-  //Enable scrolling when menu is close
-  bodyScrollLockUpgrade.enableBodyScroll(body);
 
   // set transition value to none when menu is closed
   setTimeout(() => {
     navLinkContainer.style.transition = "none";
   }, 500);
 }
-setUpNav(media);
+setUpNav(media); //run after whole content is loaded
 
 btnOpen.addEventListener("click", openMobileMenu);
 btnClose.addEventListener("click", closeMobileMenu);
 
 media.addEventListener("change", function (e) {
   setUpNav(e);
+});
+
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (media.matches) {
+      closeMobileMenu();
+    }
+  });
 });
