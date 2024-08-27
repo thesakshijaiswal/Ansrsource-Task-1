1c25c97d55261b6e95fe882c4eeefd4de0695399
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-27 11:10:59 +0530

    feat: Make hamburger menu functional using aria expanded attribute

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const btnOpen = document.querySelector("#btn-open");
+const btnClose = document.querySelector("#btn-close");
+
+function openMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+}
+function closeMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+}
+
+btnOpen.addEventListener('click', openMobileMenu)
+btnClose.addEventListener('click', closeMobileMenu)
