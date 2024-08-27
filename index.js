85ffd99b102a8911045c578048819975b8d54b9f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-27 12:41:24 +0530

    fix: Hide small screen menu from accessibility tools for screen size above 900px

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const btnOpen = document.querySelector("#btn-open");
 const btnClose = document.querySelector("#btn-close");
 
-function openMobileMenu() {
-  btnOpen.setAttribute('aria-expanded', 'true');
-}
-function closeMobileMenu() {
-  btnOpen.setAttribute('aria-expanded', 'false');
+//Show small screen menu to accessibility tools only when the below screen size matches
+const media = window.matchMedia('(width < 900px)');
+const navLinkContainer = document.querySelector('.nav-link-container');
+const main = document.querySelector('main');
+
+function setUpNav(e){
+  if(e.matches){
+    //is mobile
+    console.log("Is mobile");
+    navLinkContainer.setAttribute('inert', '');
+    // set transition value to none when screen size changes
+    navLinkContainer.style.transition = 'none';
+  }
+  else{
+    //is tablet/desktop
+    console.log("is desktop")
+    navLinkContainer.removeAttribute('inert');
+  }
 }
 
-btnOpen.addEventListener('click', openMobileMenu)
-btnClose.addEventListener('click', closeMobileMenu)
+// Open menu
+function openMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  navLinkContainer.removeAttribute('inert');
+  navLinkContainer.removeAttribute('style');
+  main.setAttribute('inert', '');
+}
+
+// Close menu
+function closeMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  navLinkContainer.setAttribute('inert', '');
+  main.removeAttribute('inert');
+
+  // set transition value to none when menu is closed
+  setTimeout(() => {
+    navLinkContainer.style.transition = 'none';
+  },500)
+}
+
+setUpNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+media.addEventListener('change', function (e) {
+  setUpNav(e);
+})
